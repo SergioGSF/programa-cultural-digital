7ed793affbbcaea9817d0d3626eb66011ebbdb14
Samara020 <smn@cesar.school>
2026-04-03 08:41:15 -0300

    Update CadastroController.java

--- arenapernambuco/src/main/java/br/com/projeto/arenapernambuco/controller/CadastroController.java
+++ arenapernambuco/src/main/java/br/com/projeto/arenapernambuco/controller/CadastroController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RegisterController {
-    @GetMapping("/register")
-    public String register() {
-        return "register";
+public class CadastroController {
+    @GetMapping("/cadastro")
+    public String cadastro() {
+        return "cadastror";
     }
 }
